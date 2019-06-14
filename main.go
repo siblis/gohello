bb7ed9a2f95810e7ec2fb07c7d576dd17014168f
sergei <siblis@yandex.ru>
2019-06-14 22:39:46 +0300

    add title to homework

--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
-import "gohello/right"
+import (
+	"fmt"
+	"gohello/right"
+)
 
 func main() {
-
+	fmt.Println("My first homework")
 	right.Println(right.Execute | right.Read)
 	right.Println(right.Execute | right.Read | right.Write)
 	right.Println(right.Execute | right.Write)
