41006901aff2880e61d91503c6c27a45c33e35f4
sergei <siblis@yandex.ru>
2019-06-21 16:21:45 +0300

    calc

---
+++ calc.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func calc() {
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Println(r)
+		}
+	}()
+
+	println("Простой калькулятор. Для выхода введите exit")
+
+	for {
+		// Ввод первого числа
+		firstNum := enterNumber("Введите первое число: ")
+
+		action := inputData("Укажите действие: ")
+
+		// Ввод второго числа
+		secondNum := enterNumber("Введите второе число: ")
+		if secondNum == 0 {
+			panic("division by zero")
+		}
+
+		fmt.Printf("Результат: %v \n", calculate(firstNum, action, secondNum))
+	}
+}
+
+func calculate(firstNum float32, action string, secondNum float32) float32 {
+	switch action {
+	case "+":
+		return firstNum + secondNum
+	case "-":
+		return firstNum - secondNum
+	case "*":
+		return firstNum * secondNum
+	case "/":
+		return firstNum / secondNum
+	default:
+		fmt.Println("Не удалось распознать действие")
+		return 0
+	}
+}
+
+func inputData(msg string) (data string) {
+	fmt.Print(msg)
+	fmt.Scanln(&data)
+	if data == "exit" {
+		panic(data)
+	}
+	return data
+}
+
+func enterNumber(msg string) float32 {
+	for {
+		num, err := strconv.ParseFloat(inputData(msg), 32)
+		if err == nil {
+			// Ввод числа прошел без ошибок
+			return float32(num)
+		}
+		fmt.Println("Не удалось распознать число")
+	}
+}
