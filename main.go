0a6002875d76328143c873d16c7fa734cd42b6c0
sergei <siblis@yandex.ru>
2019-06-21 16:22:20 +0300

    calc

--- main.go
+++ main.go
@@ -1,13 +1,6 @@
+//#!/usr/bin/go run
 package main
 
-import (
-	"fmt"
-	"gohello/right"
-)
-
 func main() {
-	fmt.Println("My first homework")
-	right.Println(right.Execute | right.Read)
-	right.Println(right.Execute | right.Read | right.Write)
-	right.Println(right.Execute | right.Write)
+	calc()
 }
