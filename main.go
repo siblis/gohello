27ff643451401e78b0ba03fe1a2025c06496e475
sergei <siblis@yandex.ru>
2019-06-24 22:53:16 +0300

    calc v2

--- main.go
+++ main.go
@@ -1,5 +1,27 @@
 package main
 
+import (
+	"fmt"
+	"gohello/calculator"
+)
+
 func main() {
-	myerrordemo()
+	input := ""
+	for {
+		fmt.Print("> ")
+		if _, err := fmt.Scanln(&input); err != nil {
+			fmt.Println(err)
+			continue
+		}
+
+		if input == "exit" {
+			break
+		}
+
+		if res, err := calculator.Calculate(input); err == nil {
+			fmt.Printf("Результат: %v\n", res)
+		} else {
+			fmt.Println("Не удалось произвести вычисление")
+		}
+	}
 }
